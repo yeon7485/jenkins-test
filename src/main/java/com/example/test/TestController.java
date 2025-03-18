eac2d259b3a5a7e805d87bdc326dd4f8805f09b9
yeon7485 <wsx4379@naver.com>
2025-03-18 11:39:03 +0900

    first commit

---
+++ src/main/java/com/example/test/TestController.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/ex")
+    public String ex01() {
+        return "v2";
+    }
+}
